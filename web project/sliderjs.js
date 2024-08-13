2fafddee5316d17de8cebfda587dfeeed32d0c47
Sudhiksha-Mahesh <130494967+Sudhiksha-Mahesh@users.noreply.github.com>
2024-08-13 15:31:12 +0530

    Add files via upload

---
+++ web project/sliderjs.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slider-images")[0].getElementsByTagName("img");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// Automatic slide change
+setInterval(() => {
+  changeSlide(1);
+}, 5000); // Change slide every 5 seconds
